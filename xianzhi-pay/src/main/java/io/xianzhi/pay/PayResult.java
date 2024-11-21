0a9802d17e9c8b1fe1467ad51752be671bcaf975
Max <max@xianzhi.io>
2024-11-21 08:52:24 +0800

    add payResult

---
+++ xianzhi-pay/src/main/java/io/xianzhi/pay/PayResult.java
@@ -0,0 +1,11 @@
+package io.xianzhi.pay;
+
+/**
+ * Marker interface for payment results.
+ * This interface can be used to represent the result of a payment operation.
+ *
+ * @author Max
+ * @since 1.0.0
+ */
+public interface PayResult {
+}
