62fb134006f8625dc7aed81a60eb11b20dd472a7
Max <max@xianzhi.io>
2025-03-07 09:02:51 +0800

    init

---
+++ xianzhi-system/system-model/src/main/java/io/xianzhi/system/model/enums/UserStatusEnum.java
@@ -0,0 +1,10 @@
+package io.xianzhi.system.model.enums;
+
+/**
+ * 用户状态枚举
+ *
+ * @author Max
+ * @since 1.0.0
+ */
+public enum UserStatusEnum {
+}
---
+++ xianzhi-system/system-model/src/main/java/io/xianzhi/system/model/vo/UserVO.java
@@ -0,0 +1,15 @@
+package io.xianzhi.system.model.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 用户信息出参
+ *
+ * @author Max
+ * @since 1.0.0
+ */
+@Data
+public class UserVO implements Serializable {
+}
