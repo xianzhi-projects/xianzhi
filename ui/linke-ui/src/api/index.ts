60d9f08fa265f6c4869fd17cce3d612ca9d341cc
Max <max@xianzhi.io>
2025-03-07 10:13:07 +0800

    init

--- ui/linke-ui/src/api/index.ts
+++ ui/linke-ui/src/api/index.ts
@@ -14,22 +14,3 @@
  *  limitations under the License.
  */
 
-import {createRouter, createWebHistory} from 'vue-router'
-import Dashboard from '../views/dashboard/Index.vue'
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'dashboard',
-      component: Dashboard,
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      component: () => import('@/views/error/404.vue'),
-    },
-  ],
-})
-
-export default router
