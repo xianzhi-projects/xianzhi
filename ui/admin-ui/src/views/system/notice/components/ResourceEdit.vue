d10cbea15554fc7040ee3b6ad80639905a0c5be4
Max <max@xianzhi.io>
2025-03-18 11:42:42 +0800

    init

---
+++ ui/admin-ui/src/views/system/notice/components/ResourceEdit.vue
@@ -0,0 +1,11 @@
+<template>
+  <a-card style="width: 100%;height: 1000px" title="菜单信息">
+    <TreeComponent/>
+  </a-card>
+</template>
+
+<script lang="ts" setup>
+
+import TreeComponent from "@/components/TreeComponent.vue";
+</script>
+<style scoped></style>
---
+++ ui/admin-ui/src/views/system/notice/components/ResourceTree.vue
@@ -0,0 +1,22 @@
+<template>
+  <a-card style="width: 100%;height: 1000px" title="">
+    <template #extra>
+      <a-button type="primary">新增菜单</a-button>
+    </template>
+    <TreeComponent/>
+  </a-card>
+</template>
+
+<script lang="ts" setup>
+import TreeComponent from "@/components/TreeComponent.vue";
+import {onMounted} from "vue";
+
+onMounted(() => {
+  initTreeData()
+  // 默认展开第一层
+  autoExpandParent.value = false
+})
+</script>
+
+
+<style scoped></style>
