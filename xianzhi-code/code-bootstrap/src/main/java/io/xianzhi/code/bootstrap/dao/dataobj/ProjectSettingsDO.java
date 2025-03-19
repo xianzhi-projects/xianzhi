cea6afbdd5feb76a6dbe343b080a3d1b5c14ce68
Max <max@xianzhi.io>
2025-03-19 13:51:30 +0800

    init

---
+++ xianzhi-code/code-bootstrap/src/main/java/io/xianzhi/code/bootstrap/dao/dataobj/ProjectSettingsDO.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright 2025 XianZhi Group .
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.xianzhi.code.bootstrap.dao.dataobj;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.xianzhi.common.mybatis.plus.base.BaseDO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 项目设置实体
+ *
+ * @author Max
+ * @since 1.0.0
+ */
+@Data
+@TableName(value = "c_project_settings")
+@EqualsAndHashCode(callSuper = true)
+public class ProjectSettingsDO extends BaseDO {
+}
