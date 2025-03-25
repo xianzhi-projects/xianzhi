c6e14b24d3739860b1af32f467765dc6a24c92e1
Max <max@xianzhi.io>
2025-03-25 10:28:59 +0800

    init

---
+++ xianzhi-code/code-bootstrap/src/main/java/io/xianzhi/code/bootstrap/resolver/XianZhiGitLocationResolver.java
@@ -0,0 +1,38 @@
+package io.xianzhi.code.bootstrap.resolver;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.sshd.git.GitLocationResolver;
+import org.apache.sshd.server.session.ServerSession;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Path;
+
+/**
+ * Git仓库加载
+ *
+ * @author Max
+ * @since 1.0.0
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class XianZhiGitLocationResolver implements GitLocationResolver {
+    /**
+     * @param command The complete received command
+     * @param args    The command split into arguments - {@code args[0]} is the &quot;pure&quot; command itself
+     *                without any other arguments. <B>Note:</B> changing the content of the arguments array may
+     *                affect command execution in undetermined ways, due to invocation code changes without prior
+     *                notice, so <U>highly recommended to avoid it</U>.
+     * @param session The {@link ServerSession} through which the command was received
+     * @param fs      The {@link FileSystem} associated with the server session
+     * @return The local GIT repository root path
+     * @throws IOException If failed to resolve
+     */
+    @Override
+    public Path resolveRootDirectory(String command, String[] args, ServerSession session, FileSystem fs) throws IOException {
+        return null;
+    }
+}
