707e28bc44c6c01a4e8c9f41d2e1dee0f8ec3500
Max <max@xianzhi.io>
2025-03-27 13:45:11 +0800

    init

--- ui/xianzhi-system/src/views/authorization/Login.vue
+++ ui/xianzhi-system/src/views/authorization/Login.vue
@@ -20,7 +20,7 @@
     <!-- 左侧图片 -->
     <div class="login-left">
       <div class="image-wrapper">
-<!--        <img src="@/assets/login-bg.jpg" alt="Login Background" />-->
+        <!--        <img src="@/assets/login-bg.jpg" alt="Login Background" />-->
         <div class="overlay">
           <h2>Vben Admin</h2>
           <p>一个高效、现代化的管理后台系统</p>
@@ -30,7 +30,7 @@
 
     <!-- 右侧登录表单 -->
     <div class="login-right">
-      <login-form />
+      <!--      <LoginForm/>-->
       <div class="copyright">
         Copyright © 2024 Vben
       </div>
@@ -38,24 +38,20 @@
   </div>
 </template>
 
-<script lang="ts" setup>
-import LoginForm from './components/LoginForm.vue';
-</script>
+<script lang="ts" setup></script>
 
 <style lang="less" scoped>
-// 定义变量
-@background-color: #fff;
-@text-color: #fff;
-@text-color-secondary: #d9d9d9;
-
 .login-page {
   display: flex;
   min-height: 100vh;
-  background: @background-color;
+  background-color: #f1f1f1;
+
   .login-left {
+    display: flex;
     flex: 2;
     position: relative;
-    background: #000;
+    background-color: #1890ff;
+
     .image-wrapper {
       position: absolute;
       top: 0;
@@ -63,6 +59,7 @@ import LoginForm from './components/LoginForm.vue';
       width: 100%;
       height: 100%;
       overflow: hidden;
+
       img {
         width: 100%;
         height: 100%;
@@ -75,52 +72,15 @@ import LoginForm from './components/LoginForm.vue';
         left: 50%;
         transform: translate(-50%, -50%);
         text-align: center;
-        color: @text-color;
-
-        h2 {
-          font-size: 36px;
-          margin-bottom: 16px;
-        }
-
-        p {
-          font-size: 16px;
-          color: @text-color-secondary;
-        }
+        color: #fff;
       }
     }
+
+
   }
 
   .login-right {
     flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 40px;
-    background: #fff;
-    position: relative;
-
-    .copyright {
-      position: absolute;
-      bottom: 10px;
-      color: @text-color-secondary;
-      font-size: 12px;
-    }
-  }
-
-  // 响应式布局
-  @media (max-width: 768px) {
-    flex-direction: column;
-
-    .login-left {
-      flex: none;
-      height: 300px;
-    }
-
-    .login-right {
-      flex: none;
-      padding: 20px;
-    }
   }
 }
 </style>
